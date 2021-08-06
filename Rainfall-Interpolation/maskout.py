a0cc2af165762c0e0f48174190f3dd56f5bd0097
Imran-D-Mahmood <48464030+Imran-D-Mahmood@users.noreply.github.com>
2021-08-06 11:54:00 +0800

    Add files via upload

--- Rainfall-Interpolation/maskout.py
+++ Rainfall-Interpolation/maskout.py
@@ -3,31 +3,70 @@
 #####This module enables you to maskout the unneccessary data outside the interest region on a matplotlib-plotted output instance
 ####################in an effecient way,You can use this script for free     ########################################################
 #####################################################################################################################################
-#####USAGE: INPUT  include           'originfig':the matplotlib instance##
+#####USAGE: INPUT  include           'originfig': the matplotlib instance##
 #                                    'ax': the Axes instance
 #                                    'shapefile': the shape file used for generating a basemap A
-#                                    'region':the name of a region of on the basemap A,outside the region the data is to be maskout
-#           OUTPUT    is             'clip' :the the masked-out or clipped matplotlib instance.
+#                                    'region': the name of a region of on the basemap A,outside the region the data is to be maskout
+#           OUTPUT    is             'clip': the the masked-out or clipped matplotlib instance.
 import shapefile
 from matplotlib.path import Path
 from matplotlib.patches import PathPatch
+
+import cartopy.crs as ccrs
+import cartopy.io.shapereader as shpreader
+from cartopy.mpl.ticker import LongitudeFormatter,LatitudeFormatter
+import cartopy.io.shapereader as shpreader
+from cartopy.io.shapereader import Reader
+from cartopy.feature import ShapelyFeature
+import geopandas
+
 def shp2clip(originfig,ax,shpfile,region):
     
-    sf = shapefile.Reader(shpfile, encoding='utf-8') # encoding utf-8 / ISO8859-1
-    for shape_rec in sf.shapeRecords():
-        if shape_rec.record[2] == region:  ####Here you need to find a unique identifier that matches the region, and one item in record[] must correspond.
+    sf = shapefile.Reader(shpfile, encoding='ISO8859-1') # encoding utf-8 / ISO8859-1
+    
+    # obtain shapes and its records
+    # the shapes are the actual coordinate points 
+    # the record contains all attributes related to each shape
+    shape_records = sf.shapeRecords()
+
+    # search for the ones with desired records
+    desired_shapes = []
+    
+    #adm1_shapes=list(shpreader.Reader(shpfile).geometries())
+    #ax.add_geometries(adm1_shapes[:], ccrs.PlateCarree(), edgecolor='k', facecolor='none', alpha=0.5)
+    
+    # read the shapefile using geopandas
+    df = geopandas.read_file(shpfile)
+
+    # read the borders
+    for i in range(0,13):
+        poly = df.loc[df['PROVINCE'] == 'Benguet']['geometry'].values[i]
+        ax.add_geometries([poly], crs=ccrs.PlateCarree(), facecolor='none', edgecolor='k', lw=0.5)
+    #ax = plt.axes(projection=ccrs.PlateCarree())
+    #for pol in [poly]:
+    #    ax.add_geometries([pol], crs=ccrs.PlateCarree(), facecolor='none', edgecolor='k')
+    #countries = sf.records()
+    #for country in countries:
+    #    if country.attributes['PROVINCE'] == "Benguet":
+    #        ax.add_geometries(country.geometry, ccrs.PlateCarree(), edgecolor='k', facecolor='none', alpha=0.5)
+    
+    for shape_rec in shape_records:
+        if shape_rec.record[4] == region:  ####Here you need to find a unique identifier that matches the region, and one item in record[] must correspond.
+            desired_shapes.append(shape_rec.shape)
             vertices = []
             codes = []
-            pts = shape_rec.shape.points
-            prt = list(shape_rec.shape.parts) + [len(pts)]
-            for i in range(len(prt) - 1):
-                for j in range(prt[i], prt[i+1]):
-                    vertices.append((pts[j][0], pts[j][1]))
-                codes += [Path.MOVETO]
-                codes += [Path.LINETO] * (prt[i+1] - prt[i] -2)
-                codes += [Path.CLOSEPOLY]
-            clip = Path(vertices, codes)
-            clip = PathPatch(clip, transform=ax.transData)
+            
+            for x in desired_shapes:
+                pts = x.points
+                prt = list(x.parts) + [len(pts)]
+                for i in range(len(prt) - 1):
+                    for j in range(prt[i], prt[i+1]):
+                        vertices.append((pts[j][0], pts[j][1]))
+                    codes += [Path.MOVETO]
+                    codes += [Path.LINETO] * (prt[i+1] - prt[i] -2)
+                    codes += [Path.CLOSEPOLY]
+                clip = Path(vertices, codes)
+                clip = PathPatch(clip, transform=ax.transData, facecolor='white')
     for contour in originfig.collections:
         contour.set_clip_path(clip)
     return clip
--- Rainfall-Interpolation/rainfall-interpolation.py
+++ Rainfall-Interpolation/rainfall-interpolation.py
@@ -12,6 +12,10 @@ import cartopy.io.shapereader as shpreader
 from cartopy.io.shapereader import Reader
 from cartopy.feature import ShapelyFeature
 import maskout  # Show only a certain area
+import cartopy
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import InsetPosition
+from shapely.geometry.polygon import LinearRing
 
 
 plt.rcParams["figure.facecolor"] = 'whitesmoke'
@@ -19,52 +23,92 @@ plt.rcParams["figure.facecolor"] = 'whitesmoke'
 fig = plt.figure(figsize=(6.5,8)) # WxH 650x800
 ax = fig.add_subplot(111, projection=ccrs.PlateCarree())
 
-plt.title('Benguet Observed Rainfall Accumulation (mm)\n05-22-21 8AM - 05-23-21 8AM (PhT)')
-plt.figtext(0.5, 0.01, 'Station data from PAGASA and DOST-ASTI | Plotted by Imran Mahmood', fontsize=8, color='black', ha='center')
+plt.title('Benguet Observed + Interpolated Rainfall Accumulation (mm)\n08-05-21 8AM - 08-06-21 8AM PhT', fontsize=10)
 
 # data preparation
-excfile = 'Benguet-Stations.xlsx'
-f = pd.read_excel(excfile, 'Sheet1').dropna()
+#excfile = 'Benguet-Stations.xlsx'
+#f = pd.read_excel(excfile, 'Sheet1').dropna()
+f = pd.read_csv('Daily-Aug05.csv').dropna()
 f.columns = ['NAME','LATITUDE','LONGITUDE','RAIN_ACC_MM']
 lon = f['LONGITUDE']
 lat = f['LATITUDE']
 rain = f['RAIN_ACC_MM']
 
 plt.scatter(lon, lat, s=8, color='lightgray', edgecolor='dimgray', zorder=4, transform=ccrs.PlateCarree())
-#for i, data in enumerate(rain):
-#    ax.annotate(' ' + str(data), (lon[i], lat[i]), fontsize=9, color='black', fontweight='bold') # repr - convert float to str w/o losing precision
+for i, data in enumerate(rain):
+	ax.annotate(' ' + str(data), (lon[i], lat[i]), fontsize=8, color='black', fontweight='bold')
+
+plt.text(120.38, 16.93, '*EXPERIMENTAL; NOT AN OFFICIAL PRODUCT*',  fontsize=9, color='red', transform=ccrs.PlateCarree());
 
 # Drawing preparation
-olon = np.linspace (120.37,120.95,120) # Long and latitude coordinates, 0.05° resolution 118° to 126° 0.05 resolution is 160 grid points
-olat = np.linspace (16.18,16.95,60) # Latitude coordinates, 0.05° resolution
+extent = [120.37, 120.95, 16.18, 16.95]
+lonmin, lonmax, latmin, latmax = extent
+olon = np.linspace (lonmin,lonmax,120) # Long and latitude coordinates, 0.05° resolution 118° to 126° 0.05 resolution is 160 grid points
+olat = np.linspace (latmin,latmax,60) # Latitude coordinates, 0.05° resolution
 olon,olat = np.meshgrid(olon,olat) # Generate coordinate grid meshgrid meshing
 func = Rbf(lon,lat,rain,function='linear') # Interpolation function - Call the cubic interpolation method in the Rbf interpolation function linear
 rain_data_new = func(olon,olat) # Interpolation
 rain_data_new[rain_data_new <0 ] = 0
 
 # Custom precipitation cmap
-clevs = [0,1,2,5,10,15,20,30,40,50,70,90,100,130,150,200,300,500]
-cdict = ['#ffffff','#98ffff','#00ceff','#009aff','#006af7','#2e9c00','#2bff00','#fefe08','#ffcb00','#ff9c00','#fe0005','#c90200','#9d0000','#9a009d','#cf00d7','#ff00f7','#fdcafe']
+clevs = [4,5,10,15,20,30,40,50,70,80,90,100,150,200,250,300,400,500]
+cdict = ['#98ffff','#00ceff','#009aff','#006af7','#2e9c00','#2bff00','#fefe08','#ffcb00','#ff9c00','#fe0005','#c90200','#9d0000','#9a009d','#cf00d7','#ff00f7','#fdcafe', 'pink', 'lightpink']
 my_cmap = colors.ListedColormap(cdict)
 norm = mpl.colors.BoundaryNorm(clevs,my_cmap.N) # Generate color mapping index based on discrete interval
 
 # Draw contours, color contours
-cf = ax.contourf(olon, olat, rain_data_new, clevs, transform=ccrs.PlateCarree(), cmap=my_cmap, norm=norm) #alpha=0.3
+cf = ax.contourf(olon, olat, rain_data_new, clevs, transform=ccrs.PlateCarree(), cmap=my_cmap, norm=norm, extend='both')
 
-# ct = ax.contour(olon,olat,rain_data_new,clevs) # draw contour
-# clabel = ax.clabel(ct,fmt = '%i')
-position = fig.add_axes([0.82,0.2,0.05,0.2]) # Position [left, bottom, width. high】
-plt.colorbar(cf, cax=position)
+#ct = ax.contour(olon,olat,rain_data_new,clevs) # draw contour
+#clabel = ax.clabel(cf, fmt='%i')
+#position = fig.add_axes([0.82,0.2,0.05,0.2]) # Position [left, bottom, width. high】
+#plt.colorbar(cf, cax=position)
+clb = plt.colorbar(cf, ticks=clevs, fraction=0.04)
+clb.ax.set_xlabel('mm')
 
-#ax.xaxis.set_major_formatter(LongitudeFormatter(zero_direction_label=True))
-#ax.yaxis.set_major_formatter(LatitudeFormatter())
+# Municipality labels
+munfs = 7
+muncol = 'black'
+munalpha = 0.8
+
+plt.text(120.5760, 16.4023, '$\it{Baguio}$', fontsize=munfs, color=muncol, alpha=munalpha)
+plt.text(120.6890, 16.3452, '$\it{Itogon}$', fontsize=munfs, color=muncol, alpha=munalpha)
+plt.text(120.5740, 16.3005, '$\it{Tuba}$', fontsize=munfs, color=muncol, alpha=munalpha)
+plt.text(120.5855, 16.4774, '$\it{La Trinidad}$', fontsize=munfs, color=muncol, alpha=munalpha)
+plt.text(120.5164, 16.4855, '$\it{Sablan}$', fontsize=munfs, color=muncol, alpha=munalpha)
+plt.text(120.6315, 16.5121, '$\it{Tublay}$', fontsize=munfs, color=muncol, alpha=munalpha)
+plt.text(120.7810, 16.4564, '$\it{Bokod}$', fontsize=munfs, color=muncol, alpha=munalpha)
+plt.text(120.5970, 16.6124, '$\it{Kapangan}$', fontsize=munfs, color=muncol, alpha=munalpha)
+plt.text(120.6890, 16.6816, '$\it{Kibungan}$', fontsize=munfs, color=muncol, alpha=munalpha)
+plt.text(120.6890, 16.5974, '$\it{Atok}$', fontsize=munfs, color=muncol, alpha=munalpha)
+plt.text(120.8269, 16.6070, '$\it{Kabayan}$', fontsize=munfs, color=muncol, alpha=munalpha)
+plt.text(120.8368, 16.7492, '$\it{Buguias}$', fontsize=munfs, color=muncol, alpha=munalpha)
+plt.text(120.6890, 16.8080, '$\it{Bakun}$', fontsize=munfs, color=muncol, alpha=munalpha)
+plt.text(120.7810, 16.8770, '$\it{Mankayan}$', fontsize=munfs, color=muncol, alpha=munalpha)
 
 # # # #Cut
-clip = maskout.shp2clip(cf, ax, 'bg2/bg2.shp', 'Philippines')
+clip = maskout.shp2clip(cf, ax, 'MuniCities/MuniCities.shp', 'Benguet')
 
-#shpname = r'MuniCities/MuniCities.shp'  # MuniCities/MuniCities.shp
-#adm1_shapes=list(shpreader.Reader(shpname).geometries())
-#ax.add_geometries(adm1_shapes[:], ccrs.PlateCarree(), edgecolor='k', facecolor='none', alpha=0.5)
+plt.figtext(0.5, 0.02, 'Station data from PAGASA and DOST-ASTI | Plotted by Imran Mahmood', fontsize=8, color='black', ha='center')
 
+ax2 = inset_axes(ax, width="35%", height="35%", loc="upper left", axes_class=cartopy.mpl.geoaxes.GeoAxes, axes_kwargs=dict(map_projection=cartopy.crs.PlateCarree()))
+ax2.add_feature(cartopy.feature.COASTLINE, lw=0.5)
+ax2.add_feature(cartopy.feature.LAND, facecolor='white')
+ax2.set_extent([116.0, 127.0, 5.0, 22.0])
+
+nvert = 100
+lons = np.r_[np.linspace(lonmin, lonmin, nvert),
+             np.linspace(lonmin, lonmax, nvert),
+             np.linspace(lonmax, lonmax, nvert)].tolist()
+lats = np.r_[np.linspace(latmin, latmax, nvert),
+             np.linspace(latmax, latmax, nvert),
+             np.linspace(latmax, latmin, nvert)].tolist()
+
+ring = LinearRing(list(zip(lons, lats)))
+ax2.add_geometries([ring], ccrs.PlateCarree(), facecolor='none', edgecolor='red', linewidth=0.75)
+ax2.set_frame_on(False)  
+
+plt.tight_layout()
 #plt.savefig('BenguetRain.png')
+
 plt.show()
